7d57fc101aaeca1da6dfc5af790f8a1c59087fd4
Furkan KARA - Junior Java Developer <58552366+Furkan9528@users.noreply.github.com>
2021-04-02 00:35:11 +0200

    Thymeleafdemo-employees-add

---
+++ 29-thymeleafdemo-employees-add/src/main/java/com/furkankara/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.furkankara.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.furkankara.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	//that's it ...no need to write any code
+}
