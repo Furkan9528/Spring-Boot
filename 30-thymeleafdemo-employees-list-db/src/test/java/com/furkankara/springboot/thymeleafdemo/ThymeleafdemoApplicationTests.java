ac7b67a2439a6c5dce3b2bb347f9189eb514ccf9
Furkan KARA - Junior Java Developer <58552366+Furkan9528@users.noreply.github.com>
2021-04-04 12:50:54 +0200

    Thymeleafdemo-employees-list-db

---
+++ 30-thymeleafdemo-employees-list-db/src/test/java/com/furkankara/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkankara.springboot.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
